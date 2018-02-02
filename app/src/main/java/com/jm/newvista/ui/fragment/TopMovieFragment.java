cfa0da93736738c8e4b134abd1678153ea8afec6
Johnny Miller <johnnysviva@outlook.com>
2018-02-03 00:12:38 +0800

    Fix bugs: layout_width="wrap_content" in card_top_movie.xml

--- app/src/main/java/com/jm/newvista/ui/fragment/TopMovieFragment.java
+++ app/src/main/java/com/jm/newvista/ui/fragment/TopMovieFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,6 +65,7 @@ public class TopMovieFragment extends BaseFragment<TopMovieModel, TopMovieView, 
         myViewPager.setAdapter(topMovieViewPagerAdapter);
         PageIndicatorView pageIndicatorView = (PageIndicatorView) view.findViewById(R.id.pageIndicatorView);
         pageIndicatorView.setViewPager(myViewPager);
+        myViewPager.setOffscreenPageLimit(4);
         pageIndicatorView.setAnimationType(AnimationType.WORM);
         return view;
     }
