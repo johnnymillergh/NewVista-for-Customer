4c613562d37a1bc68e896030d6390c816de4ed5d
Johnny Miller <johnnysviva@outlook.com>
2018-02-12 16:58:46 +0800

    ‼🌟Add message service

---
+++ app/src/main/java/com/jm/newvista/ui/activity/MessageServiceTestActivity.java
@@ -0,0 +1,15 @@
+package com.jm.newvista.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jm.newvista.R;
+
+public class MessageServiceTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_service_test);
+    }
+}
