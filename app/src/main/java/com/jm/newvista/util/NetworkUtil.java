591c6052e0f0caba435d3bfe40d9ef84d15c555e
Johnny Miller <johnnysviva@outlook.com>
2018-02-18 18:10:13 +0800

    Update apis of NetworkUtil by using domain name of my web server

--- app/src/main/java/com/jm/newvista/util/NetworkUtil.java
+++ app/src/main/java/com/jm/newvista/util/NetworkUtil.java
@@ -8,13 +8,15 @@ import com.tsy.sdk.myokhttp.MyOkHttp;
 
 public class NetworkUtil {
     public static MyOkHttp myOkHttp = new MyOkHttp();// Aliyun server ip: 39.106.218.175
-    public static final String SIGN_UP_URL = "http://39.106.218.175/servlet.customer.SignUp";
-    public static final String LOG_IN_URL = "http://39.106.218.175/servlet.customer.LogIn";
-    public static final String USER_INFO_URL = "http://39.106.218.175/servlet.customer.GetUserInfo";
-    public static final String GET_AVATAR_URL = "http://39.106.218.175/getAvatar.jsp";
-    public static final String SECONDARY_LOGON_URL = "http://39.106.218.175/servlet.customer.SecondaryLogon";
-    public static final String GET_MOVIE_URL = "http://39.106.218.175/servlet.customer.GetMovie";
-    public static final String USER_REVIEW_MANAGEMENT_URL = "http://39.106.218.175/servlet.customer.UserReviewManagement";
-    public static final String ORDER_URL = "http://39.106.218.175/servlet.customer.Order";
-    public static final String GET_TOP_MOVIE_URL = "http://39.106.218.175/servlet.customer.GetTopMovie";
+    public static final String SIGN_UP_URL = "http://johnnymiller.site/servlet.customer.SignUp";
+    public static final String LOG_IN_URL = "http://johnnymiller.site/servlet.customer.LogIn";
+    public static final String USER_INFO_URL = "http://johnnymiller.site/servlet.customer.GetUserInfo";
+    public static final String GET_AVATAR_URL = "http://johnnymiller.site/getAvatar.jsp";
+    public static final String SECONDARY_LOGON_URL = "http://johnnymiller.site/servlet.customer.SecondaryLogon";
+    public static final String GET_MOVIE_URL = "http://johnnymiller.site/servlet.customer.GetMovie";
+    public static final String GET_MOVIE_POSTER_URL = "http://johnnymiller.site/getMoviePoster.jsp";
+    public static final String GET_TOP_MOVIE_POSTER_URL = "http://johnnymiller.site/getTopMoviePoster.jsp";
+    public static final String USER_REVIEW_MANAGEMENT_URL = "http://johnnymiller.site/servlet.customer.UserReviewManagement";
+    public static final String ORDER_URL = "http://johnnymiller.site/servlet.customer.Order";
+    public static final String GET_TOP_MOVIE_URL = "http://johnnymiller.site/servlet.customer.GetTopMovie";
 }
