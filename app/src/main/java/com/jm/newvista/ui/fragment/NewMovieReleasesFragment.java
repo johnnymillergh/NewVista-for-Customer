b6521b0e223a1054713d6a717cf48b99cc8df530
Johnny Miller <johnnysviva@outlook.com>
2018-02-24 23:54:45 +0800

    Remove useless codes

--- app/src/main/java/com/jm/newvista/ui/fragment/NewMovieReleasesFragment.java
+++ app/src/main/java/com/jm/newvista/ui/fragment/NewMovieReleasesFragment.java
@@ -30,13 +30,6 @@ import java.util.List;
 public class NewMovieReleasesFragment
         extends BaseFragment<NewMovieReleasesModel, NewMovieReleasesView, NewMovieReleasesPresenter>
         implements NewMovieReleasesView {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private RecyclerView newMovieReleasesRecyclerView;
     private NewMovieReleasesRecyclerViewAdapter newMovieReleasesRecyclerViewAdapter;
     private Button more;
@@ -47,30 +40,12 @@ public class NewMovieReleasesFragment
         // Required empty public constructor
     }
 
-    public static NewMovieReleasesFragment newInstance(String param1, String param2) {
-        NewMovieReleasesFragment fragment = new NewMovieReleasesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
         if (enter) {
-            return AnimationUtils.loadAnimation(getActivity(), R.anim.fade_in_from_bottom_to_top);
+            return AnimationUtils.loadAnimation(getActivity(), R.anim.fade_in_from_right_to_left);
         } else {
-            return AnimationUtils.loadAnimation(getActivity(), R.anim.fade_in_from_bottom_to_top);
+            return AnimationUtils.loadAnimation(getActivity(), R.anim.fade_in);
         }
     }
 
