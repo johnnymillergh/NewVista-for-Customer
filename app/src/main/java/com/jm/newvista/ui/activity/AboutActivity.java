c2572079df6dd5a6d8ea2144649d561f9d69a0a2
Johnny Miller <johnnysviva@outlook.com>
2018-03-03 23:40:36 +0800

    Add AboutActivity, OrderHistoryActivity, WatchlistActivity

---
+++ app/src/main/java/com/jm/newvista/ui/activity/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.jm.newvista.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jm.newvista.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
