39b633c7ab65d5ee0b98573f910a3463c323431e
Johnny Miller <johnnysviva@outlook.com>
2018-03-04 12:40:54 +0800

    Add NowInTheatersActivity, RandomPicksActivity

---
+++ app/src/main/java/com/jm/newvista/ui/activity/NowInTheatersActivity.java
@@ -0,0 +1,15 @@
+package com.jm.newvista.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jm.newvista.R;
+
+public class NowInTheatersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_in_theaters);
+    }
+}
