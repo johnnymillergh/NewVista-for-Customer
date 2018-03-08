c9c0ccc78a0a03b1ed117a7d4e71d691cbf65d46
Johnny Miller <johnnysviva@outlook.com>
2018-03-08 19:48:42 +0800

    ㊗Enhance UI design of ticket detail

--- app/src/main/java/com/jm/newvista/ui/activity/TicketDetailActivity.java
+++ app/src/main/java/com/jm/newvista/ui/activity/TicketDetailActivity.java
@@ -1,15 +1,34 @@
 package com.jm.newvista.ui.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.jm.newvista.R;
 
 public class TicketDetailActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_detail);
+        initView();
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
