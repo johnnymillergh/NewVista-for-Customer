ad87f90e42ca49fc9143b7c666bae938a113cd82
Johnny Miller <johnnysviva@outlook.com>
2018-03-17 15:40:04 +0800

    Add MovieRatingEntity.java

---
+++ app/src/main/java/com/jm/newvista/bean/MovieRatingEntity.java
@@ -0,0 +1,22 @@
+package com.jm.newvista.bean;
+
+public class MovieRatingEntity {
+    private String title;
+    private float average_score;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getAverage_score() {
+        return average_score;
+    }
+
+    public void setAverage_score(float average_score) {
+        this.average_score = average_score;
+    }
+}
