b33152ee4c8eae9ff7c0b3ec5d612916dfd85527
Johnny Miller <johnnysviva@outlook.com>
2018-03-24 14:40:20 +0800

    Add module: TheaterFragment

---
+++ app/src/main/java/com/jm/newvista/ui/fragment/TheaterFragment.java
@@ -0,0 +1,59 @@
+package com.jm.newvista.ui.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jm.newvista.R;
+
+public class TheaterFragment extends Fragment {
+    private TheaterFragmentListener mListener;
+
+    public TheaterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_theater, container, false);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof TheaterFragmentListener) {
+            mListener = (TheaterFragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface TheaterFragmentListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
