ab1b6a2dbd4e37c9c32306df120e90c0677d68fb
Johnny Miller <johnnysviva@outlook.com>
2018-05-15 17:59:10 +0800

    Update server APIs

--- app/src/main/java/com/jm/newvista/util/NetworkUtil.java
+++ app/src/main/java/com/jm/newvista/util/NetworkUtil.java
@@ -8,30 +8,31 @@ import com.tsy.sdk.myokhttp.MyOkHttp;
 
 public class NetworkUtil {
     public static MyOkHttp myOkHttp = new MyOkHttp();// Aliyun server ip: 39.106.218.175
-    public static final String SIGN_UP_URL = "http://johnnymiller.site/servlet.customer.SignUp";
-    public static final String LOG_IN_URL = "http://johnnymiller.site/servlet.customer.LogIn";
-    public static final String USER_INFO_URL = "http://johnnymiller.site/servlet.customer.GetUserInfo";
-    public static final String GET_AVATAR_URL = "http://johnnymiller.site/customer/getAvatar.jsp?id=";
-    public static final String SECONDARY_LOGON_URL = "http://johnnymiller.site/servlet.customer.SecondaryLogon";
-    public static final String GET_MOVIE_URL = "http://johnnymiller.site/servlet.customer.GetMovie";
-    public static final String GET_MOVIE_POSTER_URL = "http://johnnymiller.site/customer/getMoviePoster.jsp?title=";
+    private static final String BASE_URL = "http://140.143.144.123/";
+    public static final String SIGN_UP_URL = BASE_URL + "servlet.customer.SignUp";
+    public static final String LOG_IN_URL = BASE_URL + "servlet.customer.LogIn";
+    public static final String USER_INFO_URL = BASE_URL + "servlet.customer.GetUserInfo";
+    public static final String GET_AVATAR_URL = BASE_URL + "customer/getAvatar.jsp?id=";
+    public static final String SECONDARY_LOGON_URL = BASE_URL + "servlet.customer.SecondaryLogon";
+    public static final String GET_MOVIE_URL = BASE_URL + "servlet.customer.GetMovie";
+    public static final String GET_MOVIE_POSTER_URL = BASE_URL + "customer/getMoviePoster.jsp?title=";
     public static final String
-            GET_TOP_MOVIE_POSTER_URL = "http://johnnymiller.site/customer/getTopMoviePoster.jsp?movieTitle=";
+            GET_TOP_MOVIE_POSTER_URL = BASE_URL + "customer/getTopMoviePoster.jsp?movieTitle=";
     public static final String
-            USER_REVIEW_MANAGEMENT_URL = "http://johnnymiller.site/servlet.customer.UserReviewManagement";
-    public static final String ORDER_URL = "http://johnnymiller.site/servlet.customer.Order";
-    public static final String GET_TOP_MOVIE_URL = "http://johnnymiller.site/servlet.customer.GetTopMovie";
-    public static final String GET_MOVIE_SCHEDULE_URL = "http://johnnymiller.site/servlet.customer.GetMovieSchedule";
-    public static final String GET_THEATER_LOGO_URL = "http://johnnymiller.site/customer/getTheaterLogo.jsp?id=";
+            USER_REVIEW_MANAGEMENT_URL = BASE_URL + "servlet.customer.UserReviewManagement";
+    public static final String ORDER_URL = BASE_URL + "servlet.customer.Order";
+    public static final String GET_TOP_MOVIE_URL = BASE_URL + "servlet.customer.GetTopMovie";
+    public static final String GET_MOVIE_SCHEDULE_URL = BASE_URL + "servlet.customer.GetMovieSchedule";
+    public static final String GET_THEATER_LOGO_URL = BASE_URL + "customer/getTheaterLogo.jsp?id=";
     public static final String
-            GET_LOWEST_PRICE_URL = "http://johnnymiller.site/customer/getLowestPrice.jsp?movieTitle=";
-    public static final String GET_LOWEST_PRICE_URL2 = "http://johnnymiller.site/customer/getLowestPrice.jsp?movieId=";
-    public static final String GET_NOW_IN_THEATERS_URL = "http://johnnymiller.site/customer/getNowInTheaters.jsp";
-    public static final String SAT_MANAGEMENT_URL = "http://johnnymiller.site/servlet.customer.SATManagement";
-    public static final String GET_TOP_RATED_URL = "http://johnnymiller.site/customer/getTopRated.jsp";
-    public static final String GET_MOVIE_SCHEDULE_URL2 = "http://johnnymiller.site/customer/getMovieSchedule.jsp";
-    public static final String GET_TOP_SELLING_URL = "http://johnnymiller.site/customer/getTopSelling.jsp";
+            GET_LOWEST_PRICE_URL = BASE_URL + "customer/getLowestPrice.jsp?movieTitle=";
+    public static final String GET_LOWEST_PRICE_URL2 = BASE_URL + "customer/getLowestPrice.jsp?movieId=";
+    public static final String GET_NOW_IN_THEATERS_URL = BASE_URL + "customer/getNowInTheaters.jsp";
+    public static final String SAT_MANAGEMENT_URL = BASE_URL + "servlet.customer.SATManagement";
+    public static final String GET_TOP_RATED_URL = BASE_URL + "customer/getTopRated.jsp";
+    public static final String GET_MOVIE_SCHEDULE_URL2 = BASE_URL + "customer/getMovieSchedule.jsp";
+    public static final String GET_TOP_SELLING_URL = BASE_URL + "customer/getTopSelling.jsp";
     public static final String
-            WATCHLIST_MANAGEMENT_URL = "http://johnnymiller.site/servlet.customer.WatchlistManagement";
-    public static final String GET_GROSS_URL = "http://johnnymiller.site/customer/getGross.jsp";
+            WATCHLIST_MANAGEMENT_URL = BASE_URL + "servlet.customer.WatchlistManagement";
+    public static final String GET_GROSS_URL = BASE_URL + "customer/getGross.jsp";
 }
